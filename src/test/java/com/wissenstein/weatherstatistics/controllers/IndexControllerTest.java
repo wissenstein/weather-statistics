2ad7ed5a203abeb65335bd3c7bb4eb751dc1ede7
Oleksil Lupandin <wissenstein@gmail.com>
2015-08-13 03:19:09 +0300

    Corrected IndexControllerTest.

--- src/test/java/com/wissenstein/weatherstatistics/controllers/IndexControllerTest.java
+++ src/test/java/com/wissenstein/weatherstatistics/controllers/IndexControllerTest.java
@@ -3,12 +3,15 @@ package com.wissenstein.weatherstatistics.controllers;
 import com.wissenstein.weatherstatistics.domain.DailyStatistics;
 import com.wissenstein.weatherstatistics.persistence.WeatherRepository;
 import com.wissenstein.weatherstatistics.service.WeatherService;
+import com.wissenstein.weatherstatistics.util.Date;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -25,13 +28,43 @@ public class IndexControllerTest {
 
     private static final String REQUESTED_DATE = "2015-08-02";
 
+    private DailyStatistics expectedTemperatureByDate;
+
+    @Before
+    public void setUp() {
+        expectedTemperatureByDate = new DailyStatistics();
+        expectedTemperatureByDate.setDate(Date.parse(REQUESTED_DATE));
+        expectedTemperatureByDate.setNightTemperature(16);
+        expectedTemperatureByDate.setMorningTemperature(15);
+        expectedTemperatureByDate.setDayTemperature(24);
+        expectedTemperatureByDate.setEveningTemperature(25);
+    }
+
     @Test
     public void getWeatherFromServiceWhenNotFoundInDatabase()
     throws Exception {
+        when(weatherRepository.getTemperatureByDate(REQUESTED_DATE))
+                .thenReturn(null);
+        when(weatherService.getTemperatureByDate(REQUESTED_DATE))
+                .thenReturn(expectedTemperatureByDate);
+
         controller.getWeatherFromService(REQUESTED_DATE);
 
-        verify(weatherRepository).getTemperatureByDate(REQUESTED_DATE);
-        verify(weatherService).getTemperatureByDate(REQUESTED_DATE);
-        verify(weatherRepository).insertTemperature(any(DailyStatistics.class));
+        verify(weatherRepository).insertTemperature(expectedTemperatureByDate);
+    }
+
+    @Test
+    public void getWeatherFromServiceWhenFoundInDatabase() throws Exception {
+        when(weatherRepository.getTemperatureByDate(REQUESTED_DATE))
+                .thenReturn(expectedTemperatureByDate);
+
+        final DailyStatistics temperature
+                = controller.getWeatherFromService(REQUESTED_DATE);
+
+        assertEquals(expectedTemperatureByDate, temperature);
+        verify(weatherService, never())
+                .getTemperatureByDate(anyString());
+        verify(weatherRepository, never())
+                .insertTemperature(any(DailyStatistics.class));
     }
 }
